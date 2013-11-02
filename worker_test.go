bf8f69863e7e41405ea46b44fc476a5a0f0eb923
Clarke Retzer <clarke@carfoo.com>
2013-11-01 21:29:54 -0700

    Making the test func more localized

--- worker_test.go
+++ worker_test.go
@@ -4,20 +4,18 @@ import (
     "testing"
 )
 
-var resultArray = make([]string, 0)
-
-func AddOneToTheEndOfString(l string) {
-    resultArray = append(resultArray, l)
-}
-
 func TestProcessStringArray(t *testing.T) {
     lines := []string{"one", "two", "three"}
+
+    var resultArray = make([]string, 0)
+    WorkerFunc = func (l string) {
+        resultArray = append(resultArray, l)
+    }
 
     if len(resultArray) != 0 {
         t.Error("Not starting with a clean resultArray")
     }
 
-    WorkerFunc = AddOneToTheEndOfString
     ProcessStringArray(lines)
 
     if len(resultArray) != len(lines) {
@@ -28,14 +26,7 @@ func TestProcessStringArray(t *testing.T) {
 func TestDefaultWokerFunc(t *testing.T) {
     lines := []string{"one", "two", "three"}
 
-    // Make sure to reset results so we can test to make sure it didn't do anything
-    resultArray = []string{}
-
     // Set WorkerFunc back to the default
     WorkerFunc = stringNoop
     ProcessStringArray(lines)
-
-    if len(resultArray) > 0 {
-        t.Error("ProcessStringArray populated when it wasn't supposed to", resultArray)
-    }
 }
